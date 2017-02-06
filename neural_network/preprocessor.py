3b1b85a0e9f96d99c99c7e9f3c0c63f40829106e
Kris Vukasinovic <axellerate@hotmail.com>
2017-02-05 20:01:04 -0500

    began updating neural network

--- neural_network/preprocessor.py
+++ neural_network/preprocessor.py
@@ -9,21 +9,21 @@ class Preprocessor:
 	def __init__(self):
 		self.lemmatizer = WordNetLemmatizer()
 		self.number_of_lines = 1000000
-		self.data_files = ["data/polar_data.txt",
-					  	   "data/forests_data.txt",
-					       "data/oceans_data.txt",
-					       "data/air_data.txt"]
-		self.classifications = [{"polar":[1,0,0,0,0],
+		self.data_files = {"polar":"data/polar_data.txt",
+					  	   "forests":"data/forests_data.txt",
+					       "oceans":"data/oceans_data.txt",
+					       "air":"data/air_data.txt",
+					       "none":"data/random_text_data.txt"}
+		self.classifications = {"polar":[1,0,0,0,0],
 								 "forests":[0,1,0,0,0],
 								 "oceans":[0,0,1,0,0],
 								 "air":[0,0,0,1,0],
 								 "none":[0,0,0,0,1]}
-								]
 		self.lexicon = []
 
 	def create_lexicon(self):
 		lexicon = []
-		for file in self.data_files:
+		for f_type,file in self.data_files.items():
 			with open(file,'r') as f:
 				contents = f.readlines()
 				for line in contents[:self.number_of_lines]:
@@ -33,12 +33,13 @@ class Preprocessor:
 		word_count = Counter(lexicon)
 		result = []
 		for w in word_count:
-			if 1000 > word_count[w] > 5:
+			if 1000 > word_count[w] > 50:
 				result.append(w)
 		self.lexicon = result
+		print("Lexicon of size {} successfully created".format(len(self.lexicon)))
 		return self.lexicon
 
-	def sample_handling(sample,lexicon,classification):
+	def sample_handling(self,sample,lexicon,classification):
 		featureset = []
 		with open(sample,'r') as f:
 			contents = f.readlines()
@@ -52,7 +53,44 @@ class Preprocessor:
 						features[index_value] += 1
 				features = list(features)
 				featureset.append([features, classification])
+		print("Featureset generated: {}".format(classification))
 		return featureset
 
-	def create_feature_sets_and_labels():
-		pass
+	def create_feature_sets_and_labels(self,test_size=0.1):
+		lexicon = self.create_lexicon()
+		features = []
+		features += self.sample_handling(self.data_files['polar'],
+									lexicon,
+									self.classifications['polar'])
+		features += self.sample_handling(self.data_files['forests'],
+									lexicon,
+									self.classifications['forests'])
+		features += self.sample_handling(self.data_files['oceans'],
+									lexicon,
+									self.classifications['oceans'])
+		features += self.sample_handling(self.data_files['air'],
+									lexicon,
+									self.classifications['air'])
+		features += self.sample_handling(self.data_files['none'],
+									lexicon,
+									self.classifications['none'])
+		random.shuffle(features)
+
+		features = np.array(features)
+		testing_size = int(test_size*len(features))
+
+		print("Testing size: {}".format(testing_size))
+
+		train_x = list(features[:,0][:-testing_size])
+		train_y = list(features[:,1][:-testing_size])
+
+		test_x = list(features[:,0][-testing_size:])
+		test_y = list(features[:,1][-testing_size:])
+
+		return train_x,train_y,test_x,test_y
+
+
+
+
+
+
--- wikipedia_parser/wikipedia_parser.py
+++ wikipedia_parser/wikipedia_parser.py
@@ -22,6 +22,9 @@ class WikipediaParser:
 	def get_air_training_pages(self):
 		return self.training_pages['air-pages']
 
+	def get_random_text_training_pages(self):
+		return self.training_pages['random-pages']
+
 	def fill_polar_data(self):
 		os.remove('data/polar_data.txt')
 		polar_pages = self.get_polar_training_pages()
@@ -32,6 +35,7 @@ class WikipediaParser:
 			data += self.get_page(page).content
 		polar_file.write(data)
 		polar_file.close()
+		print("Polar text data loaded.")
 		return len(polar_pages)
 
 	def fill_forests_data(self):
@@ -44,6 +48,7 @@ class WikipediaParser:
 			data += self.get_page(page).content
 		forests_file.write(data)
 		forests_file.close()
+		print("Forests text data loaded.")
 		return len(forests_pages)
 
 	def fill_oceans_data(self):
@@ -56,6 +61,7 @@ class WikipediaParser:
 			data += self.get_page(page).content
 		oceans_file.write(data)
 		oceans_file.close()
+		print("Oceans text data loaded.")
 		return len(oceans_pages)
 
 	def fill_air_data(self):
@@ -68,7 +74,21 @@ class WikipediaParser:
 			data += self.get_page(page).content
 		air_file.write(data)
 		air_file.close()
+		print("Air text data loaded.")
 		return len(air_pages)
+
+	def fill_random_text_data(self):
+		os.remove('data/random_text_data.txt')
+		random_text_pages = self.get_random_text_training_pages()
+		random_text_file = open('data/random_text_data.txt', 'w')
+		data = ""
+		for page in random_text_pages:
+			print(self.get_page(page).title)
+			data += self.get_page(page).content
+		random_text_file.write(data)
+		random_text_file.close()
+		print("Random text data loaded.")
+		return len(random_text_pages)
 
 	def fill_all_data_files(self):
 		file_count = 0
@@ -76,6 +96,7 @@ class WikipediaParser:
 		file_count += self.fill_forests_data()
 		file_count += self.fill_oceans_data()
 		file_count += self.fill_air_data()
+		file_count += self.fill_random_text_data()
 		print("All {} data files have been successfully filled.".format(file_count))
 
 	def get_page(self,page):
